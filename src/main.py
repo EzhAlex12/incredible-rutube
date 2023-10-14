629c8d203699aba8b0f99a685355ddadff5e5ef2
Алексей Пронькин <pron.alexis@gmail.com>
2023-10-14 22:23:30 +0700

    primary publishing with notebooks

---
+++ src/main.py
@@ -0,0 +1,6 @@
+from diffusers import DiffusionPipeline
+import torch
+import os
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM
+from transformers import pipeline
